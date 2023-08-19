128c77b12cfcd8cb429abaa72f323b3362fa984b
Home PC <hansmbangu@gmail.com>
2023-08-19 15:59:37 +0200

    Add typed routes

---
+++ app/missing.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+/*** Once project is complete please rename the file to [...missing].tsx this will disable typed routs as al invalid routes com here */
+
+interface Props {}
+
+export default function MissingScreen({}: Props) {
+  return (
+    <View>
+      <Text>Page Not Found</Text>
+    </View>
+  );
+}
